61b96fd931af89244133ebd3500f1317c3196572
Clark Winkelmann <clark.winkelmann@gmail.com>
2018-04-05 03:16:18 +0200

    Increase middleware priority to only catch exceptions no other extension handles

--- src/Listeners/Middlewares.php
+++ src/Listeners/Middlewares.php
@@ -14,7 +14,10 @@ class Middlewares
 {
     public function subscribe(Dispatcher $events)
     {
-        $events->listen(ConfigureMiddleware::class, [$this, 'configureMiddleware']);
+        // It is crucial this listener is registered before any middleware that relies on exceptions
+        // Because otherwise it would prevent any of those from catching exceptions themselves
+        // This way the error handling only happens if really no other extension catches it
+        $events->listen(ConfigureMiddleware::class, [$this, 'configureMiddleware'], 10);
     }
 
     public function configureMiddleware(ConfigureMiddleware $event)
