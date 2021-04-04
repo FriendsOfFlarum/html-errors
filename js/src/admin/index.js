eca5b302218acd2b962f60ed96fd0bc5f862d4e2
Clark Winkelmann <clark.winkelmann@gmail.com>
2021-04-05 01:29:54 +0200

    Update for Flarum beta 16

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,6 +1,20 @@
 import app from 'flarum/app';
-import ErrorSettingsPage from './components/ErrorSettingsPage';
 
-app.initializers.add('fof-html-errors', app => {
-    app.extensionData.for('fof-html-errors').registerPage(ErrorSettingsPage);
+// Keeping the old flagrow settings prefix because it's easier
+const settingsPrefix = 'flagrow-html-errors.';
+const translationPrefix = 'fof-html-errors.admin.settings.';
+
+app.initializers.add('fof-html-errors', (app) => {
+    app.extensionData.for('fof-html-errors').registerSetting(function () {
+        return [403, 404, 500, 503].map((error) => (
+            <div className="Form-group">
+                <label>{app.translator.trans(translationPrefix + 'error.' + error)}</label>
+                <textarea
+                    className="FormControl"
+                    bidi={this.setting(settingsPrefix + 'custom' + error + 'ErrorHtml')}
+                    placeholder={app.translator.trans(translationPrefix + 'placeholder.empty_for_default')}
+                ></textarea>
+            </div>
+        ));
+    });
 });
