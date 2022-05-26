1a891da48a29db7e047aea2130ee756a2e9dc714
Ian Morland <ian@morland.me>
2022-05-26 20:04:25 +0100

    Webpack 5, format js

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -5,16 +5,16 @@ const settingsPrefix = 'flagrow-html-errors.';
 const translationPrefix = 'fof-html-errors.admin.settings.';
 
 app.initializers.add('fof-html-errors', (app) => {
-    app.extensionData.for('fof-html-errors').registerSetting(function () {
-        return [403, 404, 500, 503].map((error) => (
-            <div className="Form-group">
-                <label>{app.translator.trans(translationPrefix + 'error.' + error)}</label>
-                <textarea
-                    className="FormControl"
-                    bidi={this.setting(settingsPrefix + 'custom' + error + 'ErrorHtml')}
-                    placeholder={app.translator.trans(translationPrefix + 'placeholder.empty_for_default')}
-                ></textarea>
-            </div>
-        ));
-    });
+  app.extensionData.for('fof-html-errors').registerSetting(function () {
+    return [403, 404, 500, 503].map((error) => (
+      <div className="Form-group">
+        <label>{app.translator.trans(translationPrefix + 'error.' + error)}</label>
+        <textarea
+          className="FormControl"
+          bidi={this.setting(settingsPrefix + 'custom' + error + 'ErrorHtml')}
+          placeholder={app.translator.trans(translationPrefix + 'placeholder.empty_for_default')}
+        ></textarea>
+      </div>
+    ));
+  });
 });
